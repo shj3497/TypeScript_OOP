8a18c57d0db7f672d490db44f253dff552edbcfa
shj3497 <shj3497@gmail.com>
2021-07-09 21:42:28 +0900

    function

---
+++ projects/TypeScript_Type/1-2-function.ts
@@ -0,0 +1,30 @@
+{
+  // JavaScript
+  function jsAdd(num1, num2){
+    return num1 + num2;
+  }
+
+  // TypeScript
+  function tsAdd(num1: number, num2: number): number{
+    return num1 + num2;
+  }
+
+  
+  // JavaScript
+  function jsFetchNum(id){
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    })
+  }
+
+  // TypeScript
+  function tsFetchNum(id: string): Promise<number> {
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    })
+  }
+}

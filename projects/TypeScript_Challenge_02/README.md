883a2310e4a9ef4ca8c444addfb223ac09f362df
shj3497 <shj3497@gmail.com>
2021-07-25 21:36:13 +0900

    TypeScript_Challenge_02

---
+++ projects/TypeScript_Challenge_02/README.md
@@ -0,0 +1,7 @@
+# TypeScript_Challenge_02
+
+TypeScript로 Stack을 구현해보기!
+
+Stack : LIFO(Last In First Out)  
+push() : 스텍에 쌓인다.  
+pop() : 스텍에서 빼온다.
---
+++ projects/TypeScript_Challenge_02/stack.ts
@@ -0,0 +1,57 @@
+{
+  interface Stack {
+    readonly size: number;
+    push(value: string): void;
+    pop(): void;
+  }
+
+  type StackNode = {
+    readonly value: string; //? 현재 입력된 값이 있는 곳
+    readonly next?: StackNode //? 다음 노드를 가르키는 곳
+  }
+
+  class StackImpl implements Stack {
+
+    private _size: number = 0;
+    private head?: StackNode
+
+    constructor(private capacity: number){}
+    get size() {
+      return this._size;
+    }
+
+    push(value: string){
+      if(this.size === this.capacity){
+        throw new Error('Stack is Full!');
+      }
+
+      const node: StackNode = {
+        value: value,
+        next: this.head
+      }
+      this.head = node;
+      // console.log(this.head); //? 하단 주석 참고
+      this._size++;
+    }
+
+    pop(): string{
+      if(this.head == null){
+        throw new Error('Stack is Empty!');
+      }
+
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+
+      return node.value;
+    }
+  }
+
+  const stack = new StackImpl(5);
+  stack.push('HyeokJin 1');
+  stack.push('HyeokJin 2');
+  stack.push('HyeokJin 3');
+  while(stack.size !== 0){
+    console.log(stack.pop());
+  }
+}
